e8cef6d10841c8655f8ccc73c1dc6ceb75539657
James Holgate <jamesalexholgate@gmail.com>
2023-08-04 17:46:21 +0100

    Added guess validation
    Former-commit-id: 4d372c13ada55193fd2d7a5b6cc313bd3c41f3db

--- guesslist/static/guess.js
+++ guesslist/static/guess.js
@@ -2,18 +2,44 @@
 document.querySelector('.guess-form__button').addEventListener('click', function (event) {
 	event.preventDefault();
 	// TODO ensure guesses are unique
-	// For each fieldset
-	document.querySelectorAll('.guess-form__fieldset').forEach(fieldset => {
-		// Create an empty array to store the values in
-		let values = [];
-		// For each input in the fieldset with given class
-		fieldset.querySelectorAll('.guess-form__input').forEach(input => {
-			// Push value to array
-			values.push(input.value);
+	let guessesValid = true;
+	let values = [];
+	const guessInputs = document.querySelectorAll('.guess-form__input.select');
+
+	for (const input of guessInputs) {
+		val = input.value;
+		if (val == 'Select:') {
+			guessesValid = false;
+			showError('Add a guess for each song');
+			break;
+		} else if (values.includes(val)) {
+			guessesValid = false;
+			showError('Guesses must be unique');
+			break;
+		} else {
+			values.push(val);
+		}
+	}
+
+	function showError(message) {
+		document.querySelector('.js-error').classList.remove('hidden');
+		document.querySelector('.js-error__text').innerHTML = message;
+	}
+
+	if (guessesValid) {
+		// For each fieldset
+		document.querySelectorAll('.guess-form__fieldset').forEach(fieldset => {
+			// Create an empty array to store the values in
+			let values = [];
+			// For each input in the fieldset with given class
+			fieldset.querySelectorAll('.guess-form__input').forEach(input => {
+				// Push value to array
+				values.push(input.value);
+			});
+			// Set value of hidden input to string containing all values
+			fieldset.querySelector('.guess-form__input--all-values').value = values.toString();
 		});
-		// Set value of hidden input to string containing all values
-		fieldset.querySelector('.guess-form__input--all-values').value = values.toString();
-	});
-	// Submit the form
-	document.querySelector('#guess-form').submit();
+		// Submit the form
+		document.querySelector('#guess-form').submit();
+	}
 });
