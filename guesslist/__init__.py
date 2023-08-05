c2c5a2a09486365081a90ee18fa97dc352db261c
James Holgate <jamesalexholgate@gmail.com>
2023-08-05 10:45:51 +0100

    Load from config.py on local, from env on prod
    Former-commit-id: ede69a3027c56d10b8c6f9499f87f7fbd7bb974c

--- guesslist/__init__.py
+++ guesslist/__init__.py
@@ -8,17 +8,14 @@ hashids = Hashids()
 mail = Mail()
 
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping()
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=False)
+    if os.environ["FLASK_ENV"] == "production":
+        app.config.from_prefixed_env()
     else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        app.config.from_pyfile("config.py", silent=False)
 
     # ensure the instance folder exists
     try:
